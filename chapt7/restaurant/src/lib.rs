fe54a79dc603f315d9558184f888ea526bdc0d52
damilola <damiloladolor@gmail.com>
2021-12-06 16:03:23 +0100

    finished chapter 7 about rust's  module system

---
+++ chapt7/restaurant/src/lib.rs
@@ -0,0 +1,58 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
+
+mod front_of_house;
+
+fn serve_order() {}
+
+mod back_of_house {
+    fn fix_incorrect_order() {
+        cook_order();
+        super::serve_order();
+    }
+
+    fn cook_order() {}
+q
+    pub enum Appetizer {
+        Soup(String),
+        Salad,
+    }
+
+
+
+    pub struct Breakfast {
+        pub toast: String, // public
+        seasonal_fruit: String, //private
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+}
+
+// use crate::front_of_house::hosting;
+pub use crate::front_of_house::hosting; // relative path
+
+pub fn eat_at_restaurant() {
+    // Absolute path
+    hosting::add_to_waitlist();
+
+    // Relative path
+    // We are in the root path already, can skip crate
+    front_of_house::hosting::add_to_waitlist();
+
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+}
